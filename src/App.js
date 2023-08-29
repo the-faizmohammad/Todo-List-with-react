2483345ba92528b95b45554936108ddcc252a0b3
user-faiz <email2faizm@gmail.com>
2023-08-29 11:14:48 +0530

    update App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+import TodosLogic from './components/todosLogic';
+
+const TodoApp = () => (
+  <div className="wrapper">
+    <div className="todos">
+      <Header />
+      <TodosLogic />
+    </div>
+  </div>
+);
+export default TodoApp;
